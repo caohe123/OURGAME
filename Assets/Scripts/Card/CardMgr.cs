43c58bcbc97843aa4c2abdf39cdd156e053d6b16
Rainbow <zyx3219428988@163.com>
2026-03-23 13:24:50 +0800

    载入初始框架
    只需覆盖Asset文件即可

---
+++ Assets/Scripts/Card/CardMgr.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardMgr
+{
+	private static CardMgr instance;
+
+	public static CardMgr Instance {
+		get;
+		private set;
+	}
+
+
+}
